a282bcbed0c4340a092909fa5d3ab27693276e0a
Yiming <ytang3@gradcenter.cuny.edu>
2018-05-14 12:37:57 -0400

    initial plugin

---
+++ edu.cuny.hunter.log.tests/resources/testFindLocations/in/A.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class A {
+
+	private static final Logger LOGGER = Logger.getLogger("confLogger");
+
+	public static void main(String[] args) throws SecurityException, IOException {
+
+		LOGGER.info("Logger Name: " + LOGGER.getName());
+		LOGGER.warning("Can cause ArrayIndexOutOfBoundsException");
+
+		// An array of size 3
+		int[] a = { 1, 2, 3 };
+		int index = 4;
+
+		LOGGER.config("index is set to " + index);
+
+		try {
+			System.out.println(a[index]);
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			LOGGER.log(Level.SEVERE, "Exception occur", ex);
+		}
+
+	}
+
+}
---
+++ edu.cuny.hunter.log.tests/tests/edu/cuny/hunter/log/ui/tests/LoggingEvolutionTest.java
@@ -0,0 +1,84 @@
+package edu.cuny.hunter.log.ui.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.ui.tests.refactoring.Java18Setup;
+import org.eclipse.jdt.ui.tests.refactoring.RefactoringTest;
+import edu.cuny.hunter.log.core.analysis.LogAnalyzer;
+
+@SuppressWarnings("restriction")
+public class LoggingEvolutionTest extends RefactoringTest {
+
+	public LoggingEvolutionTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public static Test suite() {
+		return setUpTest(new TestSuite(LoggingEvolutionTest.class));
+	}
+
+	public static Test setUpTest(Test test) {
+		return new Java18Setup(test);
+	}
+
+	private static boolean compiles(String source, Path path) throws IOException {
+		return compiles(source, path.resolve("bin/p/A.java"));
+	}
+
+	@Override
+	protected ICompilationUnit createCUfromTestFile(IPackageFragment pack, String className) throws Exception {
+
+		System.out.println(System.getProperty("user.dir") + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+		ICompilationUnit unit = super.createCUfromTestFile(pack, className);
+
+		if (!unit.isStructureKnown())
+			throw new IllegalArgumentException(className + " has structural errors.");
+
+		Path directory = Paths.get(unit.getParent().getParent().getResource().getLocation().toString());
+
+		assertTrue("Should compile the testing cases:", compiles(unit.getSource(), directory));
+
+		return unit;
+	}
+
+	public void testFindLocations() throws Exception {
+
+		IPackageFragment p = getPackageP();
+
+		// compute the actual results.
+		ICompilationUnit cu = createCUfromTestFile(getPackageP(), "A");
+
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setResolveBindings(true);
+		parser.setSource(cu);
+
+		ASTNode ast = parser.createAST(new NullProgressMonitor());
+
+		LogAnalyzer loggingAnalyer = new LogAnalyzer();
+		ast.accept(loggingAnalyer);
+
+		loggingAnalyer.analyze();
+
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
