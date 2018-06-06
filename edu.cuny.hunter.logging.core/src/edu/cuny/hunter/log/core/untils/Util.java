35b80e25800dcc5cba498968ba41d1fc212fd745
Yiming <ytang3@gradcenter.cuny.edu>
2018-06-06 12:22:56 -0400

    method binding and argument

--- edu.cuny.hunter.logging.core/src/edu/cuny/hunter/log/core/untils/Util.java
+++ edu.cuny.hunter.logging.core/src/edu/cuny/hunter/log/core/untils/Util.java
@@ -69,12 +69,18 @@ public final class Util {
 	 * e.g. each time we call method entering, a logging record which has "FINER"
 	 * level is created.
 	 * 
-	 * @param methodName
-	 *            the name of method
+	 * @param node
 	 * @return logging level
 	 */
-	public static Level isLoggingMethod(String methodName) {
-		// TODO: method could be used to find more logging level
+	public static Level isLogExpression(MethodInvocation node) {
+		IMethodBinding methodBinding = node.resolveMethodBinding();
+
+		if (methodBinding == null
+				|| !methodBinding.getDeclaringClass().getQualifiedName().equals("java.util.logging.Logger"))
+			return null;
+
+		String methodName = methodBinding.getName();
+
 		if (methodName.equals("config"))
 			return Level.CONFIG;
 		if (methodName.equals("fine"))
@@ -90,21 +96,51 @@ public final class Util {
 		if (methodName.equals("warning"))
 			return Level.WARNING;
 
+		List<Expression> arguments = node.arguments();
+		if (arguments.size() == 0)
+			return null;
+		String firstArgument = arguments.get(0).toString();
+
 		// TODO: may need wala?
 		// They should not be null
-		if (methodName.equals("log")) 
-			return null;
-		if (methodName.equals("logp"))
-			return null;
-		if (methodName.equals("logrb"))
-			return null;
-		if (methodName.equals("setLevel"))
-			return null;
+		if (methodName.equals("log")) {
+			Level loggingLevel = getLogLevel(firstArgument);
+			if (loggingLevel == null)
+				System.out.println("Need to process LogRecord.");
+			return loggingLevel;
+		}
+		if (methodName.equals("logp")) {
+			Level loggingLevel = getLogLevel(firstArgument);
+			return loggingLevel;
+		}
+		if (methodName.equals("logrb")) {
+			Level loggingLevel = getLogLevel(firstArgument);
+			return loggingLevel;
+		}
 
-		// TODO: the handler can contain logging level
-		if (methodName.equals("addHandler"))
-			return null;
+		return null;
+	}
 
+	/**
+	 * Return a corresponding logging level of a string
+	 * @param argument
+	 * @return
+	 */
+	public static Level getLogLevel(String argument) {
+		if (argument.equals("Level.SEVERE"))
+			return Level.SEVERE;
+		if (argument.equals("Level.WARNING"))
+			return Level.WARNING;
+		if (argument.equals("Level.INFO"))
+			return Level.INFO;
+		if (argument.equals("Level.CONFIG"))
+			return Level.CONFIG;
+		if (argument.equals("Level.FINE"))
+			return Level.FINE;
+		if (argument.equals("Level.FINER"))
+			return Level.FINER;
+		if (argument.equals("Level.FINEST"))
+			return Level.FINEST;
 		return null;
 	}
 
