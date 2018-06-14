11fe67a0f6ac79ebe016062903f37953dff21f3f
Yiming <ytang3@gradcenter.cuny.edu>
2018-06-14 17:24:24 -0400

    add existing DOI model

--- edu.cuny.hunter.log.core/src/edu/cuny/hunter/log/core/analysis/LogInvocation.java
+++ edu.cuny.hunter.log.core/src/edu/cuny/hunter/log/core/analysis/LogInvocation.java
@@ -1,5 +1,6 @@
 package edu.cuny.hunter.log.core.analysis;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,8 +12,15 @@ import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.internal.corext.dom.ASTNodes;
+import org.eclipse.mylyn.context.core.ContextCore;
+import org.eclipse.mylyn.context.core.IInteractionContext;
+import org.eclipse.mylyn.context.core.IInteractionContextManager;
 import org.eclipse.mylyn.internal.context.core.DegreeOfInterest;
-
+import org.eclipse.mylyn.internal.context.core.InteractionContext;
+import org.eclipse.mylyn.internal.context.core.InteractionContextScaling;
+import org.eclipse.mylyn.internal.tasks.core.TaskList;
+import org.eclipse.mylyn.internal.tasks.ui.TasksUiPlugin;
+import org.eclipse.mylyn.monitor.core.InteractionEvent;
 import edu.cuny.hunter.log.core.utils.LoggerNames;
 
 @SuppressWarnings("restriction")
@@ -20,7 +28,7 @@ public class LogInvocation {
 
 	private final MethodInvocation expression;
 	private final Level logLevel;
-	
+
 	private static DegreeOfInterest degreeOfInterest;
 
 	private static final Logger LOGGER = Logger.getLogger(LoggerNames.LOGGER_NAME);
@@ -28,6 +36,36 @@ public class LogInvocation {
 	public LogInvocation(MethodInvocation logExpression, Level loggingLevel) {
 		this.expression = logExpression;
 		this.logLevel = loggingLevel;
+		this.degreeOfInterest = getDegreeOfInterest();
+	}
+
+	public TaskList getTaskList() {
+		return TasksUiPlugin.getTaskList();
+	}
+
+	/**
+	 * Get DOI
+	 */
+	public static DegreeOfInterest getDegreeOfInterest() {
+
+		final InteractionContext mockContext = new InteractionContext("doitest", new InteractionContextScaling());
+		DegreeOfInterest doi = new DegreeOfInterest(mockContext, ContextCore.getCommonContextScaling());
+		List<InteractionEvent> eventList = getEvents();
+		eventList.forEach(e -> {
+			doi.addEvent(e);
+		});
+
+		return doi;
+	}
+
+	/**
+	 * Get a list of interaction events
+	 */
+	public static List<InteractionEvent> getEvents() {
+		IInteractionContextManager iContextManager = org.eclipse.mylyn.context.core.ContextCore.getContextManager();
+		IInteractionContext iContext = iContextManager.getActiveContext();
+		List<InteractionEvent> eventsBeforeStrip = iContext.getInteractionHistory();
+		return eventsBeforeStrip;
 	}
 
 	public MethodInvocation getExpression() {
@@ -41,7 +79,7 @@ public class LogInvocation {
 	public MethodDeclaration getEnclosingMethodDeclaration() {
 		return (MethodDeclaration) ASTNodes.getParent(this.getExpression(), ASTNode.METHOD_DECLARATION);
 	}
-	
+
 	/**
 	 * Through the enclosing type, I can type FQN
 	 */
@@ -68,7 +106,7 @@ public class LogInvocation {
 	public int getStartPosition() {
 		return this.expression.getStartPosition();
 	}
-	
+
 	public Level getLogLevel() {
 		return this.logLevel;
 	}
