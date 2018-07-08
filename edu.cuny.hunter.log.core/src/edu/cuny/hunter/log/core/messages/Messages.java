313d80c054133f540a47b31c26ce9e1fe21c5b60
Yiming <ytang3@gradcenter.cuny.edu>
2018-07-08 15:47:56 -0400

    basic ui

--- edu.cuny.hunter.log.core/src/edu/cuny/hunter/log/core/messages/Messages.java
+++ edu.cuny.hunter.log.core/src/edu/cuny/hunter/log/core/messages/Messages.java
@@ -5,7 +5,7 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "edu.cuny.hunter.logging.core.messages.messages"; //$NON-NLS-1$
 	public static final String NoOptimizableLog = null;
-	public static String Name;
+	public static final String Name = "Optimize Logging Level!";
 	public static String CreatingChange;
 
 	static {
