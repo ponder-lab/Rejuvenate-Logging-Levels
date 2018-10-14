d9b29cc437f074119355a75969c162f88393215a
Yiming <ytang3@gradcenter.cuny.edu>
2018-10-14 16:24:58 -0400

    get the file

--- edu.cuny.hunter.github.core/src/edu/cuny/hunter/github/core/analysis/Test.java
+++ edu.cuny.hunter.github.core/src/edu/cuny/hunter/github/core/analysis/Test.java
@@ -1,8 +1,6 @@
 package edu.cuny.hunter.github.core.analysis;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
@@ -12,16 +10,23 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.diff.EditList;
+import org.eclipse.jgit.errors.CorruptObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.patch.FileHeader;
 import org.eclipse.jgit.patch.HunkHeader;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.AbstractTreeIterator;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 
 @SuppressWarnings("restriction")
 public class Test {
@@ -43,8 +48,8 @@ public class Test {
 				String logMessage = commit.getFullMessage();
 				System.out.println("LogMessage: " + logMessage);
 
-				AbstractTreeIterator oldTreeIterator = getCanonicalTreeParser(previousCommit, git);
-				AbstractTreeIterator newTreeIterator = getCanonicalTreeParser(commit, git);
+				AbstractTreeIterator oldTreeIterator = getCanonicalTreeParser(previousCommit, repo);
+				AbstractTreeIterator newTreeIterator = getCanonicalTreeParser(commit, repo);
 
 				// each diff entry is corresponding to a file
 				final List<DiffEntry> diffs = git.diff().setOldTree(oldTreeIterator).setNewTree(newTreeIterator).call();
@@ -55,21 +60,20 @@ public class Test {
 					formatter.scan(oldTreeIterator, newTreeIterator);
 
 					for (DiffEntry diffEntry : diffs) {
+
 						FileHeader fileHeader = formatter.toFileHeader(diffEntry);
 
 						// add a file
 						if (diffEntry.getChangeType().name().equals("ADD")) {
 							System.out.println("ADD: " + diffEntry.getNewPath());
 							System.out.println();
-
-							// delete a file
-						} else if (diffEntry.getChangeType().name().equals("DELETE")) {
+						} else // delete a file
+						if (diffEntry.getChangeType().name().equals("DELETE")) {
 							System.out.println("DELETE: ");
 							System.out.println(diffEntry.getOldPath());
 							System.out.println();
 
-							// modify a file
-						}
+						} // modify a file
 						if (diffEntry.getChangeType().name().equals("MODIFY")) {
 							System.out.println("----------------------------------");
 							System.out.println("MODIFY: " + diffEntry.getNewPath());
@@ -86,6 +90,9 @@ public class Test {
 								}
 
 							}
+
+							getHistoricalFile(commit, repo, diffEntry.getNewPath());
+
 							System.out.println();
 						}
 
@@ -100,22 +107,34 @@ public class Test {
 		git.close();
 	}
 
-	private static void printFileContent(String path) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(path));
-		String line = null;
-		while ((line = br.readLine()) != null) {
-			System.out.println(line);
-		}
-	}
-
-	private static AbstractTreeIterator getCanonicalTreeParser(ObjectId commitId, Git git) throws IOException {
-		try (RevWalk walk = new RevWalk(git.getRepository())) {
+	private static AbstractTreeIterator getCanonicalTreeParser(ObjectId commitId, Repository repo) throws IOException {
+		try (RevWalk walk = new RevWalk(repo)) {
 			RevCommit commit = walk.parseCommit(commitId);
 			ObjectId treeId = commit.getTree().getId();
-			try (ObjectReader reader = git.getRepository().newObjectReader()) {
+			try (ObjectReader reader = repo.newObjectReader()) {
 				return new CanonicalTreeParser(null, reader, treeId);
 			}
 		}
+	}
+
+	/**
+	 * Given the commit, repository and the path of the file, get the file.
+	 */
+	private static void getHistoricalFile(RevCommit commit, Repository repo, String path)
+			throws MissingObjectException, IncorrectObjectTypeException, CorruptObjectException, IOException {
+		RevTree tree = commit.getTree();
+		@SuppressWarnings("resource")
+		TreeWalk treeWalk = new TreeWalk(repo);
+		treeWalk.addTree(tree);
+		treeWalk.setRecursive(true);
+		treeWalk.setFilter(PathFilter.create(path));
+		if (!treeWalk.next()) {
+			return;
+		}
+		ObjectId objectId = treeWalk.getObjectId(0);
+		ObjectLoader loader = repo.open(objectId);
+
+		loader.copyTo(System.out);
 	}
 
 }
